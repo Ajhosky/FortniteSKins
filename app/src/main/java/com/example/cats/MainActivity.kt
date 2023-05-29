6f8ac936aad35f6255db4c8469b96db270ebb021
Ajosky <domin20050601@gmail.com>
2023-05-29 14:20:55 +0200

    zrobione

--- app/src/main/java/com/example/cats/MainActivity.kt
+++ app/src/main/java/com/example/cats/MainActivity.kt
@@ -3,12 +3,10 @@ package com.example.cats
 import Adapter1
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
-import android.util.Log
 import androidx.core.widget.addTextChangedListener
 import androidx.recyclerview.widget.GridLayoutManager
 import androidx.recyclerview.widget.RecyclerView
 import com.example.cats.databinding.ActivityMainBinding
-import kotlin.math.log
 import kotlin.random.Random
 
 
@@ -22,17 +20,22 @@ class MainActivity : AppCompatActivity() {
         setContentView(binding.root)
         val skins: List<Int> = listOf(R.drawable.kuno, R.drawable.polar_peely, R.drawable.potassius_peel, R.drawable.shady_doggo, R.drawable.ramirez_redux, R.drawable.surf_witch);
         val textViewNames: List<String> = listOf("kuno", "polar peely", "potassius peel", "shady doggo", "ramirez redux", "surf witch");
-        val names: ArrayList<Int> = ArrayList()
-        val btn6names: ArrayList<Int> = ArrayList()
+        val skinImage: ArrayList<Int> = ArrayList()
+        val skinName: ArrayList<String> = ArrayList()
+        val btn6Images: ArrayList<Int> = ArrayList()
+        val btn6Names: ArrayList<String> = ArrayList()
         var randomInt = 0;
-        var myAdapter = Adapter1(names, textViewNames)
+        var myAdapter = Adapter1(skinImage, skinName)
         binding.recyclerCats.layoutManager = GridLayoutManager(binding.recyclerCats.context, 1, RecyclerView.VERTICAL, false)
         binding.btn1.setOnClickListener(){
-            btn6names.clear()
-            btn6names.addAll(names)
+            btn6Images.clear()
+            btn6Names.clear()
+            btn6Images.addAll(skinImage)
+            btn6Names.addAll(skinName)
 
             randomInt  = Random.nextInt(0,5)
-            names.add(skins[randomInt])
+            skinImage.add(skins[randomInt])
+            skinName.add(textViewNames[randomInt])
 
             binding.recyclerCats.adapter = myAdapter
         }
@@ -44,48 +47,61 @@ class MainActivity : AppCompatActivity() {
             binding.recyclerCats.layoutManager = GridLayoutManager(binding.recyclerCats.context, cols.toInt(), RecyclerView.VERTICAL, false)
         }
         binding.btn2.setOnClickListener(){
-            btn6names.clear()
-            btn6names.addAll(names)
-            names.clear()
+            btn6Images.clear()
+            btn6Names.clear()
+            btn6Images.addAll(skinImage)
+            btn6Names.addAll(skinName)
+
+            skinImage.clear()
+            skinName.clear()
 
             binding.recyclerCats.adapter = myAdapter
 
         }
         binding.btn3.setOnClickListener(){
-            btn6names.clear()
-            btn6names.addAll(names)
-            if (names.size > 0){
-                names.removeAt(0)
+            btn6Images.clear()
+            btn6Names.clear()
+            btn6Images.addAll(skinImage)
+            btn6Names.addAll(skinName)
+            if (skinImage.size > 0){
+                skinImage.removeAt(0)
+                skinName.removeAt(0)
                 binding.recyclerCats.adapter = myAdapter
             }
 
         }
 
         binding.btn4.setOnClickListener(){
-            btn6names.clear()
-            btn6names.addAll(names)
-            if (names.size > 0) {
-                names.removeAt(names.size - 1)
-
+            btn6Images.clear()
+            btn6Names.clear()
+            btn6Images.addAll(skinImage)
+            btn6Names.addAll(skinName)
+            if (skinImage.size > 0) {
+                skinImage.removeAt(skinImage.size - 1)
+                skinName.removeAt(skinImage.size - 1)
                 binding.recyclerCats.adapter = myAdapter
             }
 
         }
 
         binding.btn5.setOnClickListener(){
-            btn6names.clear()
-            btn6names.addAll(names)
-            if (names.size > 0) {
-                randomInt = Random.nextInt()
-                names.removeAt(randomInt)
-
+            btn6Images.clear()
+            btn6Names.clear()
+            btn6Images.addAll(skinImage)
+            btn6Names.addAll(skinName)
+            if (skinImage.size > 0) {
+                randomInt = Random.nextInt(0,skinImage.size)
+                skinImage.removeAt(randomInt)
+                skinName.removeAt(randomInt)
                 binding.recyclerCats.adapter = myAdapter
             }
         }
 
         binding.btn6.setOnClickListener(){
-            names.clear()
-            names.addAll(btn6names)
+            skinImage.clear()
+            skinName.clear()
+            skinName.addAll(btn6Names)
+            skinImage.addAll(btn6Images)
             binding.recyclerCats.adapter = myAdapter
         }
 
